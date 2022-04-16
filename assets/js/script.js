d0e730e96360798dfcd35965daef69cada09eeb0
vengster <veng.lee@gmail.com>
2022-04-16 09:43:31 -0700

    main logic figured out. need to clean code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,116 +1,62 @@
 // // Assignment code here
-// //
-// var uCase=["A","B","C","D","E","F","G","H","I","J","L"];
-// var lCase=["a","b","c","d","e","f"];
-// var sChar=["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^","_","`","{","|","}","~"];
-// var num=["0","1","2","3","4","5","6","7","8","9"];
+
+var uCase=["A","B","C","D","E","F","G","H","I","J","L"];
+var lCase=["a","b","c","d","e","f"];
+var symbol=["!","#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","^","_","`","{","|","}","~"];
+var num=["0","1","2","3","4","5","6","7","8","9"];
 
 
 
-var randomFunc = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-}
+// function to generate password
 
-//var lengthEl = document.getElementById('length');
+function generatePassword() {
 
-
+  // push() user inputs from window promts into empty array.
+  var userInput = [];
+  // placeholder string for generated password
+  var password = "";
 
 
-
-function getRandomLower(){
-  return String.fromCharCode(Math.floor(Math.random()*26) + 97);
-}
-console.log(getRandomLower());
-
-function getRandomUpper(){
-  return String.fromCharCode(Math.floor(Math.random()*26) + 65);
-}
-console.log(getRandomUpper());
-
-function getRandomNumber(){
-  return String.fromCharCode(Math.floor(Math.random()*10) + 48);
-}
-console.log(getRandomNumber());
-
-function getRandomSymbol(){
-  return String.fromCharCode(Math.floor(Math.random()*15) + 33);
-}
-console.log(getRandomSymbol());
-
-
-
-
-
-
-var lengthChoice = window.prompt("How long would you like your password to be?");
-  if (lengthChoice < 8 || lengthChoice > 128) {
+  // prompt user to enter password length between 8 and 128 characters.
+  var lengthChoice = window.prompt("How long would you like your password to be?");
+  if (lengthChoice < 8 || lengthChoice > 128 || lengthChoice === null || lengthChoice === "") {
     window.alert("Please choose between 1 and 128 characters");
+    return lengthChoice;
   }
+
+  // once user inputs number within range, use window.cofirm to log other criteria using else, if.
+  // push input to empty array.
   else{
-    console.log(lengthChoice);
-}
-
-
-var confirmLcase = window.confirm("Would you like to include lowercase letters?");
-if(confirmLcase) {
-  getRandomLower();
-  console.log(getRandomLower());
-}
-else{
-  console.log(confirmLcase);
-}
-
-var confirmUcase = window.confirm("Would you like to include uppercase letters?");
-  if(confirmUcase) {
-    getRandomUpper();
-    console.log(getRandomUpper());
+    if(window.confirm("Would you like to include lowercase letters?")){
+      Array.prototype.push.apply(userInput, lCase);
+    }
+    if(window.confirm("Would you like to include uppercase letters?")){
+      Array.prototype.push.apply(userInput, uCase);
+    }
+    if(window.confirm("Would you like to include numbers?")){
+      Array.prototype.push.apply(userInput, num);
+    }
+    if(window.confirm("Would you like to include special characters?")){
+      Array.prototype.push.apply(userInput, symbol);
+    }
+    console.log(userInput);
+    if(userInput.length === 0){
+      window.alert("You must select at least one criteria for your password. Please try again");
+    }
+    else {
+      for(var i = 0; i < lengthChoice; i++){
+        var random = Math.floor(Math.random() * userInput.length);
+        password += userInput[random];
+      }
+    }
   }
-  else{
-    console.log(confirmUcase);
-  }
-
-var confirmSymbol = window.confirm("Would you like to include special characters?");
-  if(confirmSymbol){
-    getRandomSymbol();
-    console.log(getRandomSymbol());
-  }
-  else{
-    console.log(confirmSymbol);
-  }
-    
-var confirmNumber = window.confirm("Would you like to include numbers?");
-if(confirmNumber){
-  getRandomNumber();
-  console.log(getRandomNumber());
-  }
-  else{
-    console.log(confirmNumber);
-  }
+  document.getElementById("password").innerHTML = password;
+  
+};
 
 
 
-
-
-
-
-//document.getElementById("generate")
-
-
-
-// Get references to the #generate element
+// // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
-  
-}
-  
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", generatePassword);
